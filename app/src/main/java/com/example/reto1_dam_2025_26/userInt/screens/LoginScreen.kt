7262d6763c231610cc449003b91a0cf056621ff0
nsmirnitskii-hue <n.smirnitskii@ikasle.eus>
2025-10-24 17:09:20 +0200

    feat/task18/AWS Lambda para enviar Correos

--- app/src/main/java/com/example/reto1_dam_2025_26/userInt/screens/LoginScreen.kt
+++ app/src/main/java/com/example/reto1_dam_2025_26/userInt/screens/LoginScreen.kt
@@ -79,12 +79,15 @@ fun AuthScreen(
             val message = if (showLogin) {
                 "Inicio de sesion exitoso!"
             } else {
-                "Registro exitoso!"
+                // Cuando el registro es exitoso, enviamos el correo
+                com.example.reto1_dam_2025_26.utils.enviarCorreoRegistro(state.email)
+                "Registro exitoso! Se ha enviado un correo de confirmación."
             }
             Toast.makeText(context, message, Toast.LENGTH_LONG).show()
             onLoggedIn()
         }
     }
+
 
     Box(
         modifier = Modifier
